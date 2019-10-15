e7dd55f79d50e4d3d3f67207a5716531243230b1
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-15 13:56:29 -0700

    created models,controllers and then api routes i.e backend stuff

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 app.listen(PORT, function() {
   console.log("App is listening at PORT:", PORT);
-});
-
-app.get("/", function(req, res) {
-  res.send("Hello Welcome to Express");
 });
